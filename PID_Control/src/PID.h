ceb55f32ad42f8fe270a6e58e191f5a2077df24f
SunshengGu <41024127+SunshengGu@users.noreply.github.com>
2018-12-13 22:01:07 -0500

    Create PID.h

---
+++ PID_Control/src/PID.h
@@ -0,0 +1,56 @@
+#ifndef PID_H
+#define PID_H
+
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+class PID {
+public:
+  /*
+  * Errors
+  */
+  double p_error;
+  double i_error;
+  double d_error;
+
+  /*
+  * Coefficients
+  */ 
+  double Kp;
+  double Ki;
+  double Kd;
+
+  /*
+  * Constructor
+  */
+  PID();
+
+  /*
+  * Destructor.
+  */
+  virtual ~PID();
+
+  /*
+  * Initialize PID.
+  */
+  void Init(double Kp, double Ki, double Kd);
+
+  /*
+  * Update the PID error variables given cross track error.
+  */
+  void UpdateError(double cte);
+
+  /*
+  * Calculate the total PID error.
+  */
+  double TotalError();
+  
+  /*
+  * Assign a specific control paramter
+  */
+  void assign_control_param(int index, double value);
+};
+
+#endif /* PID_H */
