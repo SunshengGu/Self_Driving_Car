0d3f8418d12e4d1d117c97317ee46fa084739420
SunshengGu <41024127+SunshengGu@users.noreply.github.com>
2019-01-02 12:20:31 -0800

    Update PID.h

--- PID_Control/src/PID.h
+++ PID_Control/src/PID.h
@@ -2,6 +2,8 @@
 #define PID_H
 
 #include <vector>
+#include <iostream>
+#include <iomanip> 
 #include <math.h>
 
 using namespace std;
@@ -21,6 +23,27 @@ public:
   double Kp;
   double Ki;
   double Kd;
+  
+  // Vector that stores the delta values for control parameters
+  vector<double> dp;
+  // Vector that stores the control parameters
+  vector<double> controls;
+  // Stop twiddling if the sum of dp values exceeds the threshold
+  double thresh;
+  // Increase by 1 for each motion step, has three element, one for each control parameters
+  vector<int> steps;
+  int step;
+  // Indicates wheter the previous run for twiddle has completed
+  bool completed;
+  // Do twiddle per this number of cycles
+  int twiddle_cycle;
+  // Initialize best_err and the sum of square of total erro, to be used for twiddle
+  double best_err;
+  double sum_square_error;
+  // Indicates which round of twiddling is being performed
+  int round;
+  // Indicates whether twiddling for all 3 parameters has been done once
+  vector<bool> done;
 
   /*
   * Constructor
@@ -51,6 +74,16 @@ public:
   * Assign a specific control paramter
   */
   void assign_control_param(int index, double value);
+  
+  /*
+  * Implement twiddle to tune parameters
+  */
+  void twiddle();
+  
+  /*
+  * Output control parameters
+  */
+  void output_param();
 };
 
 #endif /* PID_H */
